6110e53d0ba1eb930dec1cec5fc00e2b7c6303c9
rp_2709 <rp2709@hotmail.com>
2024-09-16 15:03:02 +0200

    added lots of adaptation functions tested and working

--- SoundBasis/Adapter.cpp
+++ SoundBasis/Adapter.cpp
@@ -3,26 +3,6 @@
 #include <limits>
 #include <iostream>
 
-Buffer adapt(const Buffer& origin, const Settings& originSettings, const Settings& resultSettings){
-  Buffer result = origin;
-
-  ///TODO: adapt format
-
-  ///TODO: adapt sample rate
-  if(originSettings.rate != resultSettings.rate){
-    result.S16.clear();
-    double ratio = (double)originSettings.rate / (double)resultSettings.rate;
-    double sampleNb = 0;
-    while((size_t)sampleNb < origin.size()) {
-      result.S16.push_back(origin.S16[(size_t)sampleNb]);
-      sampleNb += ratio + std::numeric_limits<double>::epsilon();
-    }
-  }
-  ///TODO: adapt stereo/mono
-
-  return result;
-}
-
 RealBuffer adapt(const Buffer& origin, const Settings& originSettings){
   RealBuffer result;
   result.reserve(origin.size() / originSettings.channels);
@@ -51,6 +31,45 @@ Buffer adapt(const RealBuffer& origin,Frequency sampleRate, SampleType resultFor
   for(auto sample : origin){
     sample = (std::abs(sample) <= 1) ? sample * shortMax : std::signbit(sample) * shortMax;
     result.S16.push_back((short)sample);
+    result.S16.push_back((short)sample);
+  }
+
+  return result;
+}
+
+Buffer monoToStereo(const Buffer& mono){
+  Buffer stereo{SampleType::S16,mono.size() * 2};
+
+  for(short sample : mono.S16){
+    stereo.S16.push_back(sample);
+    stereo.S16.push_back(sample);
+  }
+
+  return stereo;
+}
+
+Buffer S24toS16(const Buffer& origin){
+  Buffer result{SampleType::S16,origin.size()};
+  const double ratio = std::pow(2.,15) / std::pow(2.,23);
+
+  for(unsigned sample24 : origin.S24){
+     int sampleAsInt = *(int*)&sample24;
+     sampleAsInt = (sampleAsInt << 8) / 256;
+    result.S16.push_back(short(sample24 * ratio));
+  }
+
+  return result;
+}
+
+Buffer changeSampleRate(const Buffer& origin, Frequency originalRate, Frequency finalRate){
+  const Real ratio = originalRate / finalRate;
+
+  Buffer result(SampleType::S16,size_t((Real)origin.size() * ratio));
+
+  double sampleNb = 0;
+  while((size_t)sampleNb < origin.size()) {
+    result.S16.push_back(origin.S16[(size_t)sampleNb]);
+    sampleNb += ratio + std::numeric_limits<double>::epsilon();
   }
 
   return result;
--- SoundBasis/Adapter.h
+++ SoundBasis/Adapter.h
@@ -4,11 +4,15 @@
 #include "Types.h"
 #include "Buffer.h"
 
-Buffer adapt(const Buffer& origin, const Settings& originSettings, const Settings& resultSettings);
-
 RealBuffer adapt(const Buffer& origin, const Settings& originSettings);
 
 Buffer adapt(const RealBuffer& origin,Frequency sampleRate,SampleType resultFormat);
+
+Buffer monoToStereo(const Buffer& mono);
+
+Buffer S24toS16(const Buffer& origin);
+
+Buffer changeSampleRate(const Buffer& origin, Frequency originalRate, Frequency finalRate);
 
 
 #endif //WAVE_PLAYER_SOUNDBASIS_ADAPTER_H_
